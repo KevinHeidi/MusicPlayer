2c0455e7e53e69ce8ed17aaafcba7945e9e39ba6
System Administrator <korfung@gmail.com>
2015-06-11 11:20:15 +0800

    Music Player

---
+++ MusicPlayer/Channel.swift
@@ -0,0 +1,13 @@
+//
+//  Channel.swift
+//  MusicPlayer
+//
+//  Created by LUOKEFENG on 9/6/2015.
+//  Copyright (c) 2015年 LUOKEFENG. All rights reserved.
+//
+
+import UIKit
+
+class Channel: NSObject {
+   
+}
---
+++ MusicPlayer/Song.swift
@@ -0,0 +1,13 @@
+//
+//  Song.swift
+//  MusicPlayer
+//
+//  Created by LUOKEFENG on 9/6/2015.
+//  Copyright (c) 2015年 LUOKEFENG. All rights reserved.
+//
+
+import UIKit
+
+class Song: NSObject {
+   
+}
